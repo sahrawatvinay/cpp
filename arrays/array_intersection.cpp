3c610edfbc601178bf3df21637823eb7ea3230fd
vinay <vinaysahrawat183a@gmail.com>
2023-02-10 17:21:33 -0500

    random

---
+++ arrays/array_intersection.cpp
@@ -0,0 +1,18 @@
+/*
+You are given two arrays 'A' and 'B' of size 'N' and 'M' respectively. 
+Both these arrays are sorted in non-decreasing order. 
+You have to find the intersection of these two arrays.
+Intersection of two arrays is an array that consists of all the common elements occurring in both arrays.
+1. The length of each array is greater than zero.
+2. Both the arrays are sorted in non-decreasing order.
+3. The output should be in the order of elements that occur in the original arrays.
+4. If there is no intersection present then return an empty array.
+
+2
+6 4
+1 2 2 2 3 4
+2 2 3 3
+3 2
+1 2 3
+3 4  
+*/
